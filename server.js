621c4ac5e4fd5a0f9437467239ddad04d287cc25
Jerryl Chong <jerrylchong@gmail.com>
2022-09-07 19:18:45 +0800

    Add endpoints for functions

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config({ silent: true });
 
-const app = express();
-const PORT = 3000;
+const controller = require("./controller");
 
-app.get("/", (req, res) => {
-  res.send("Successful response.");
-});
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/teams", controller.getAllTeams);
+app.get("/standings", controller.getGroupStandings);
+
+app.post("/teams", controller.addTeam);
+
+app.put("/match", controller.addMatch);
 
 mongoose
   .connect(
