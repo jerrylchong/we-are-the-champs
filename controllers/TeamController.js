40ec1aba8194ac041a1889b23726de511630821e
Jerryl Chong <jerrylchong@gmail.com>
2022-09-09 22:48:19 +0800

    Access correct variable

--- controllers/TeamController.js
+++ controllers/TeamController.js
@@ -110,8 +110,10 @@ const addMatch = async (match) => {
   const secondTeam = await Team.findOne({ name: team2 });
 
   // update teams' total goals
+  console.log("first before", firstTeam.goals, team1Goals);
   firstTeam.goals += team1Goals;
   secondTeam.goals += team2Goals;
+  console.log("first after", firstTeam.goals, team1Goals);
 
   // update teams' points and WDL record
   if (team1Goals > team2Goals) {
@@ -159,11 +161,11 @@ const addMatches = async (req, res) => {
 
     // check if both teams exist
     if (!firstTeam) {
-      return res.status(404).send(`Team ${team1} not found`);
+      return res.status(404).send(`Team ${match.team1} not found`);
     }
 
     if (!secondTeam) {
-      return res.status(404).send(`Team ${team2} not found`);
+      return res.status(404).send(`Team ${match.team2} not found`);
     }
 
     // only teams in the same group play each other
